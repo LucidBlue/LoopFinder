01950defd0ee60957054859242c481bcd7c0dcdd
Bradley Sheneman <Brad@hermes.hsd1.il.comcast.net>
2014-05-23 16:08:51 -0500

    Initial Commit

---
+++ LoopFinder/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LoopFinder
+//
+//  Created by Bradley Sheneman on 5/23/14.
+//  Copyright (c) 2014 Bradley Sheneman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
